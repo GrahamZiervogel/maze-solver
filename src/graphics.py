c7c70d881840f18e050f0a6baefc14d692d88d90
GrahamZiervogel <grahamziervogel@gmail.com>
2025-02-19 19:59:25 +0000

    Add ability to draw lines

--- src/graphics.py
+++ src/graphics.py
@@ -10,24 +10,35 @@ class Window():
         self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
 
-        self.running = False
+        self.__running = False
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
         print("Window closed.")
     
     def close(self):
-        self.running = False
+        self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
 
 
 class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        
+
+
+class Line():
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 
 def main():
     width = 1920
     height = 1080
+
     window = Window(width, height)
+
+    line1 = Line(Point(0, 0), Point(width, height))
+    line2 = Line(Point(width, 0), Point(0, height))
+
+    window.draw_line(line1, "red")
+    window.draw_line(line2, "green")
+
     window.wait_for_close()
 
 
