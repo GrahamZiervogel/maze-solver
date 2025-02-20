3e4aa7f7a41445d5da4dd5784e34c60cee038d6b
GrahamZiervogel <grahamziervogel@gmail.com>
2025-02-20 20:29:05 +0000

    Set seed

--- src/main.py
+++ src/main.py
@@ -2,17 +2,22 @@ from graphics import Window
 from maze import Maze
 
 
-def main():
+def main(): 
+    screen_x = 1920
+    screen_y = 1080
+
+    window = Window(screen_x, screen_y)
+
     num_rows = 12
     num_cols = 16
     margin = 50
-    screen_x = 800
-    screen_y = 600
+
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
-    window = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window)
+    seed = 10
+    
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, window, seed)
     maze.solve()
 
     window.wait_for_close()
